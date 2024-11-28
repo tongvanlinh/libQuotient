fe3fcc89de0c36a9ae32203297e16cb9605415fe
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2024-11-28 22:13:21 +0100

    ranges_extras.h: findIndex()
    Upstreaming of a piece from Quaternion.

--- Quotient/ranges_extras.h
+++ Quotient/ranges_extras.h
@@ -39,4 +39,16 @@ inline constexpr auto findIndirect(RangeT&& range, const ValT& value, ProjT proj
     return findIndirect(std::ranges::begin(range), std::ranges::end(range), value, std::move(proj));
 }
 
+//! \brief An indexOf() alternative for any range
+//!
+//! Unlike QList::indexOf(), returns `range.size()` if \p value is not found
+template <typename RangeT, typename ValT, typename ProjT = std::identity>
+    requires std::indirectly_comparable<std::ranges::iterator_t<RangeT>, const ValT*,
+                                        std::ranges::equal_to, ProjT>
+inline auto findIndex(const RangeT& range, const ValT& value, ProjT proj = {})
+{
+    using namespace std::ranges;
+    return distance(begin(range), find(range, value, std::move(proj)));
+}
+
 }
