tongvanlinh/libQuotient

commit c1ef9310b30b1fefa789756cdaafb12d6cbcb187

Use std::unique_ptr instead of QScopedPointer
QScopedPointer is getting partially deprecated since Qt 6.1, and there
are few reasons to use it instead of std::unique_ptr these days.

Quotient/jobs/downloadfilejob.cpp
@@ -23,15 +23,16 @@ public:
     explicit Private(QString serverName, QString mediaId, const QString& localFilename)
         : serverName(std::move(serverName))
         , mediaId(std::move(mediaId))
-        , targetFile(!localFilename.isEmpty() ? new QFile(localFilename) : nullptr)
-        , tempFile(!localFilename.isEmpty() ? new QFile(targetFile->fileName() + ".qtntdownload"_L1)
-                                            : new QTemporaryFile())
+        , targetFile(!localFilename.isEmpty() ? std::make_unique<QFile>(localFilename) : nullptr)
+        , tempFile(!localFilename.isEmpty()
+                       ? std::make_unique<QFile>(targetFile->fileName() + ".qtntdownload"_L1)
+                       : std::make_unique<QTemporaryFile>())
     {}
 
     QString serverName;
     QString mediaId;
-    QScopedPointer<QFile> targetFile;
-    QScopedPointer<QFile> tempFile;
+    std::unique_ptr<QFile> targetFile;
+    std::unique_ptr<QFile> tempFile;
 
     std::optional<EncryptedFileMetadata> encryptedFileMetadata;
 };
@@ -156,8 +157,8 @@ BaseJob::Status DownloadFileJob::prepareResult()
         }
     } else {
         if (d->encryptedFileMetadata.has_value()) {
-            QScopedPointer<QFile> tempTempFile(new QTemporaryFile);
-            if (!tempTempFile->open(QFile::ReadWrite)) {
+            std::unique_ptr<QFile> tempTempFile = std::make_unique<QTemporaryFile>();
+            if (!tempTempFile->isWritable()) {
                 qCWarning(JOBS) << "Failed to open temporary file for decryption"
                                 << tempTempFile->errorString();
                 return { FileError, "Couldn't open temporary file for decryption"_L1 };

Quotient/util.h
@@ -8,10 +8,11 @@
 
 #include <QtCore/QDebug>
 #include <QtCore/QElapsedTimer>
+#include <QtCore/QFuture>
 #include <QtCore/QHashFunctions>
 #include <QtCore/QLatin1String>
+#include <QtCore/QScopedPointer>
 #include <QtCore/QUrl>
-#include <QtCore/QFuture>
 
 #include <memory>
 #include <optional>
@@ -229,6 +230,15 @@ inline std::pair<InputIt, ForwardIt> findFirstOf(InputIt first, InputIt last,
 
     return { last, sLast };
 }
+
+//! \brief Common custom deleter for std::unique_ptr and QScopedPointer
+//!
+//! Since Qt 6, this is merely an alias for QScopedPointerDeleteLater (which is suitable
+//! for std::unique_ptr too).
+using DeleteLater = QScopedPointerDeleteLater;
+
+template <std::derived_from<QObject> T>
+using QObjectHolder = std::unique_ptr<T, DeleteLater>;
 
 //! \brief An owning implementation pointer
 //!

quotest/quotest.cpp
@@ -470,12 +470,13 @@ TEST_IMPL(sendFile)
     return false;
 }
 
-void getResource(const QUrl& url, QScopedPointer<QNetworkReply, QScopedPointerDeleteLater>& r,
-                 QEventLoop& el)
+using NetworkReplyPtr = QObjectHolder<QNetworkReply>;
+
+void getResource(const QUrl& url, NetworkReplyPtr& r, QEventLoop& el)
 {
     r.reset(NetworkAccessManager::instance()->get(QNetworkRequest(url)));
     QObject::connect(
-        r.data(), &QNetworkReply::finished, &el,
+        r.get(), &QNetworkReply::finished, &el,
         [url, &r, &el] {
             if (r->error() != QNetworkReply::NoError)
                 getResource(url, r, el);
@@ -490,7 +491,7 @@ bool testDownload(const QUrl& url)
     // The actual test is separate from the download invocation to help debugging
     const auto results = QtConcurrent::blockingMapped(QVector<int>{ 1, 2, 3 }, [url](int) {
         thread_local QEventLoop el;
-        thread_local QScopedPointer<QNetworkReply, QScopedPointerDeleteLater> reply{};
+        thread_local NetworkReplyPtr reply{};
         getResource(url, reply, el);
         el.exec();
         return reply->error();

commit b4bc6015ba7d175f1ed135434fd92f1a60f2b556

Fix downloading to temporary file
Regression from c1ef9310b30b1fefa789756cdaafb12d6cbcb187, which removed
actually opening the temporary file we are about to write to.

Quotient/jobs/downloadfilejob.cpp
@@ -158,7 +158,7 @@ BaseJob::Status DownloadFileJob::prepareResult()
     } else {
         if (d->encryptedFileMetadata.has_value()) {
             std::unique_ptr<QFile> tempTempFile = std::make_unique<QTemporaryFile>();
-            if (!tempTempFile->isWritable()) {
+            if (!tempTempFile->open(QFile::WriteOnly)) {
                 qCWarning(JOBS) << "Failed to open temporary file for decryption"
                                 << tempTempFile->errorString();
                 return { FileError, "Couldn't open temporary file for decryption"_L1 };